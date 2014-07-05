4861abeb1a59f889e8535a78ab91e7ceb69534e4
Mikko Perttunen <cyndis@kapsi.fi>
2014-07-05 10:50:36 +0300

    Clean up example/test a bit

--- src/test.rs
+++ src/test.rs
@@ -1,5 +1,7 @@
 use super::*;
 
+use std::f32::consts::PI_2;
+
 #[test]
 fn test_sine() {
     let pcm = PCM::open("default", Playback, Blocking).unwrap();
@@ -7,9 +9,9 @@ fn test_sine() {
 
     let mut buf = [0.0f32, ..44100];
     for (idx, sample) in buf.as_mut_slice().mut_iter().enumerate() {
-        let phase = idx as f32 / 100.0f32 * 2.0 * 3.1415;
+        let phase = (idx as f32) / 100.0 * PI_2;
         *sample = phase.sin();
     }
 
-    pcm.write_interleaved(buf.as_slice()).ok().unwrap();
+    pcm.write_interleaved(buf.as_slice()).unwrap();
 }
